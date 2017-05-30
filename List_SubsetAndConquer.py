a63f2e205d98006ab0d42fc10a27e6fbb07e2077
Priyanka Phapale <pbphapale@gmail.com>
2017-05-30 12:35:37 -0400

    Initial commit

---
+++ List_SubsetAndConquer.py
@@ -0,0 +1,24 @@
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+
+# Print out last element from areas
+print(areas[-1])
+
+
+# Print out the area of the living room
+print(areas[5])
+
+
+#Subset and Calculate
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[7]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
---
+++ VariableAssignment.py
@@ -0,0 +1,35 @@
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+
+#Calculations with variables
+
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor = 1.10
+
+# Calculate result
+result = savings * factor ** 7
+
+# Print out result
+print(result)
+
+#Other Variable types
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+#Operation with other types
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
